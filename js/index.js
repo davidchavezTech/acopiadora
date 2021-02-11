38a1b5084e130afbeed99156cae0ee57edb7f977
David Chavez <desarrolloweb@davidchavez.online>
2021-02-10 21:27:13 -0500

    almost done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,85 @@
+menuBtn = document.querySelector('.burger')
+closeBtn = document.querySelector('.close')
+
+menuBtn.addEventListener('click', (e)=>{
+  e.target.style.display = 'none'
+  document.querySelector('.nav-wrapper').classList.add('expand-menu')
+  document.querySelector('.nav-links').classList.add('open-menu')
+})
+closeBtn.addEventListener('click', (e)=>{
+  menuBtn.style.display = 'inline-block'
+  // document.querySelector('.nav-wrapper').classList.remove('expand-menu')
+  document.querySelector('.nav-wrapper').classList.add('contract-menu')
+  document.querySelector('.nav-links').classList.remove('open-menu')
+})
+prevBtn = document.getElementById('prev-btn')
+nextBtn = document.getElementById('next-btn')
+loop = true
+
+function autoChange(){
+    setTimeout(() => {
+        if(loop ==true){
+            plusDivs(1)
+            autoChange()
+        }
+    }, 4000);
+}autoChange()
+prevBtn.addEventListener('click',()=>{
+    plusDivs(-1)
+    loop = false
+})
+nextBtn.addEventListener('click',()=>{
+    plusDivs(1)
+    loop = false
+})
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+//   debugger
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+      slideIndex = 1
+    //   x[x.length-1].classList.remove('show')
+    }
+  if (n < 1) {
+      slideIndex = x.length
+    //   x[0].classList.remove('show')
+    }
+    // debugger
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+    x[i].classList.remove('show')
+    x[i].classList.remove('hide')
+  }
+  if(slideIndex==1){
+    x[x.length-1].style.display = "block";
+    x[x.length-1].classList.remove('show')
+    x[x.length-1].classList.add('hide')
+    setTimeout(() => {
+        x[x.length-1].style.display = "none";
+    }, 300);
+  }else{
+    x[slideIndex-2].style.display = "block";
+    x[slideIndex-2].classList.remove('show')
+    x[slideIndex-2].classList.add('hide')
+    setTimeout(() => {
+        x[slideIndex-2].style.display = "none";
+      }, 200);
+  }
+  setTimeout(() => {
+    x[slideIndex-1].style.display = "block"; 
+    x[slideIndex-1].classList.add('show')
+  }, 300);
+}
+
+
+
 let peruFlag = document.querySelector('.flag-peru')
 let usFlag = document.querySelector('.flag-us')
 let language = 'es'
@@ -16,6 +98,28 @@ peruFlag.addEventListener('click', ()=>{
             $('.flag-peru').attr("src","img/flag-peru-on.svg");
             $('.flag-peru').css("opacity", "1")
         }, 350);
+
+        $('#nav-inicio').html('Inicio')
+        $('#nav-te-brindamos').html('Te Brindamos')
+        $('#nav-nosotros').html('Nosotros')
+        $('#nav-contactanos').html('Contáctanos')
+        $('#titulo-page1').html('Sabor único, exquisito y fresco')
+        $('.subtitle1').html('Productos cultivados naturalmente')
+        $('.title2').html('Te brindamos')
+        $('#palta-fuerte').html('Palta Fuerte')
+        $('#palta-hass').html('Palta Hass')
+        $('#palta-one-title').html('Palta Fuerte')
+        $('#palta-two-title').html('Palta Hass')
+        $('.info').html("Trabajamos desde julio del 2019 de la mano con comunidades de las zonas de: Lambayeque, Anchash, Lima, Ica, Arequipa, Ayacucho, Huancayo, Huancavelica y Cajamarca.")
+        $('#notepad').attr("src", "img/notepad.png")
+        $('#nosotros').html('Nosotros')
+        $('.contactate-1').html('Contáctate')
+        $('.contactate-2').html('con')
+        $('.contactate-3').html('nosotros')
+        $('#name').attr('placeholder','Nombre completo')
+        $('#email').attr('placeholder','Correo Electrónico')
+        $('#number').attr('placeholder','Teléfono')
+        $('.enviar-button').html('Enviar')
     }
     
 })
@@ -48,7 +152,7 @@ usFlag.addEventListener('click', ()=>{
         $('#palta-one-title').html('Fuerte Avocado')
         $('#palta-two-title').html('Hass Avocado')
         $('.info').html("We've been working since July, 2019 hand in hand with communities from: Lambayeque, Ancash, Lima, Ica, Arequipa, Ayacucho, Huancayo, Huancavelica and Cajamarca.")
-        $('#notepade').attr("src", "img/notepad-eng.png")
+        $('#notepad').attr("src", "img/notepad-eng.png")
         $('#nosotros').html('About us')
         $('.contactate-1').html('Get in touch')
         $('.contactate-2').html('with')
